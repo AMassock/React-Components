371f05753f52c1b695c320af65453cd890fd5a97
amassock <ajmassock@gmail.com>
2023-01-23 19:34:26 -0600

    finish up standard state button components

--- component-library/src/components/Button/Button.stories.js
+++ component-library/src/components/Button/Button.stories.js
@@ -1,7 +1,13 @@
+
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Button from './Button';
 
 storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+    .add('Default', () => <Button label="Default" />)
+    .add('Primary', () => <Button label="Primary" type="primary" />) 
+    .add('Danger', () => <Button label="Danger" type="danger" />)
+    .add('Warning', () => <Button label="Warning" type="warning" />)
+    .add('Success', () => <Button label="Success" type="success" large />)
+    .add('Large Primary', () => <Button label="Large Primary Button" type="primary" large />)
