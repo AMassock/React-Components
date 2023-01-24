fa5c7c4cbf9267936d24e398ff273a0b47df09c7
amassock <ajmassock@gmail.com>
2023-01-23 21:02:21 -0600

    start form component

---
+++ component-library/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Form.css';
+
+function Form () {
+    return(
+        <form>
+            <label>Email</label>
+            <input></input>
+        </form>
+    )
+}
+
+export default Form; 
---
+++ component-library/src/components/Form/Form.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Form from './Form';
+
+storiesOf('Button', module)
+    .add('Default', () => <Form />)
